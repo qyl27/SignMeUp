ae3834a77d893f185b6d089e2a15ab54317e27b8
USS-Shenzhou <969840795@qq.com>
2024-07-27 15:24:38 +0800

    init

---
+++ src/main/java/org/teacon/signmeup/SignMeUp.java
@@ -0,0 +1,17 @@
+package org.teacon.signmeup;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+/**
+ * @author USS_Shenzhou
+ */
+@Mod(SignMeUp.MODID)
+public class SignMeUp {
+    public static final String MODID = "sign_up";
+
+    public SignMeUp(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
