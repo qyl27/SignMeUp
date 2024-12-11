1b056327cc9e1f7d36645f237ebb6218e60e042b
USS-Shenzhou <969840795@qq.com>
2024-12-11 23:35:11 +0800

    fix

--- src/main/java/org/teacon/signmeup/hud/RefreshRate.java
+++ src/main/java/org/teacon/signmeup/hud/RefreshRate.java
@@ -10,7 +10,7 @@ public enum RefreshRate implements ITranslatable {
     EVERY_FRAME("gui.sign_up.minimap.refresh.every"),
     ANOTHER_FRAME("gui.sign_up.minimap.refresh.another"),
     TICK("gui.sign_up.minimap.refresh.tick"),
-    TICK_20("gui.sign_up.minimap.refresh.tick_20");
+    TICK_20("gui.sign_up.minimap.refresh.tick20");
 
 
     RefreshRate(String translateKey) {
